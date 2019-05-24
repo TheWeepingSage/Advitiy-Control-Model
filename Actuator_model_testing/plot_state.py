0d38430e7df5d37cc00cf22b61c690a71e6bb760
TheWeepingSage <48327362+TheWeepingSage@users.noreply.github.com>
2019-05-24 18:34:29 +0530

    Add files via upload

--- Actuator_model_testing/plot_state.py
+++ Actuator_model_testing/plot_state.py
@@ -7,10 +7,6 @@ import TorqueApplied
 import min_step_sampling
 import tc_step_sampling
 
-time1 = np.loadtxt("variable_step_time.csv", delimiter=",")
-#time2 = np.linspace(0, CONTROL_STEP, 100001, endpoint=True)
-time3 = np.linspace(0, CONTROL_STEP, 1000001, endpoint=True)
-
 sat = class_sat.Satellite(np.array([0, 0, 0, 1, 0, 0, 0]), 0)
 
 sat.setRequiredTorque(0)
@@ -22,64 +18,27 @@ voltageRequired = TorqueApplied.ctrlTorqueToVoltage(sat)
 duty_cycle = voltageRequired/3.3
 
 n = 15
+num_cycles = 4
 
-num_cycles = int(CONTROL_STEP * PWM_FREQUENCY)
-num_instants = int(num_cycles * n * 2)
-timeArr = tc_step_sampling.getTimeArr(duty_cycle)
-time4 = np.zeros(1)
+time1 = np.loadtxt("tcstep_time_array.csv", delimiter=",")
+
+timeArr = min_step_sampling.getTime(duty_cycle, 15)
+time2 = np.zeros(1)
 for i in range(0, num_cycles):
-    time4 = np.concatenate((time4, timeArr + i / PWM_FREQUENCY))
-'''
-sat = class_sat.Satellite(np.array([0, 0, 0, 1, 0, 0, 0]), 0)
-sat.setRequiredTorque(0)
-sat.setMag_i(np.array([1, 1, 1])*1e-4)
-sat.setPos(np.array([1, 1, 1]))
-sat.setVel(np.array([1, 1, 2]))
+    time2 = np.concatenate((time2, timeArr + i / PWM_FREQUENCY))
 
-voltage = TorqueApplied.ctrlTorqueToVoltage(sat)
-duty_cycle = voltage/3.3
-'''
+time3 = np.linspace(0, 0.004, 4001, endpoint=True)
 
-I0 = np.zeros(3)
+time4 = np.linspace(0, 0.004, 40001, endpoint=True)
 
+state1 = np.loadtxt("aac_test_tcstep.csv", delimiter=",")
+state2 = np.loadtxt("aac_test_min_step.csv", delimiter=",")
+state3 = np.loadtxt("aac_test_true.csv", delimiter=",")
+state4 = np.loadtxt("aac_test_true_2.csv", delimiter=",")
 
-current1 = aac.getCurrentList(duty_cycle, time1, len(time1), I0)
-#current2 = aac.getCurrentList(duty_cycle, time2, len(time2), I0)
-current3 = aac.getCurrentList(duty_cycle, time4, len(time4), I0)
-
-current2 = aac.getCurrentList(duty_cycle, time3, len(time3), I0)
-
-
-#plt.plot(time1, current1[:, 0], label="vstep 0")
-plt.plot(time3, current2[:, 0], label="uniform 0")
-plt.plot(time4, current3[:, 0], label="base 0")
-plt.xlim((-0.0002, 0.0011))
+plt.plot(time1, state1[0:125, 3], label="TC step sampling, state 3", color="red")
+plt.plot(time2, state2[0:117, 3], label="minimum step sampling, state 3", color = "yellow")
+plt.plot(time3, state3[:, 3], label="Fine sampling #1, state 3", color="blue")
+plt.plot(time4, state4[:, 3], label="Fine Sampling #2, state 3", color="green")
 plt.legend()
 plt.show()
-
-
-#plt.plot(time1, -current1[:, 1], label="vstep 1")
-plt.plot(time3, -current2[:, 1], label="uniform 1")
-plt.plot(time4, -current3[:, 1], label="base 1")
-plt.xlim((-0.0002, 0.0011))
-plt.legend()
-plt.show()
-
-
-#plt.plot(time1, current1[:, 2], label="vstep 2")
-plt.plot(time3, current2[:, 2], label="uniform 2")
-plt.plot(time4, current3[:, 2], label="base 2")
-plt.xlim((-0.0002, 0.005))
-plt.legend()
-plt.show()
-
-'''
-state_ustep = np.loadtxt("aac_test_uniform_steps.csv", delimiter=",")
-state_vstep = np.loadtxt("aac_test_variable_step.csv", delimiter=",")
-state_minstep = np.loadtxt("aac_test_min_step.csv", delimiter=",")
-plt.plot(time1, state_vstep[:, 5], label="vstep state 5")
-plt.plot(time2, state_ustep[:, 5], label="ustep state 5")
-plt.plot(time4, state_minstep[:, 5], label="minstep state 5")
-plt.legend()
-plt.show()
-'''
